1414ad019e51277e49eaa7ce9dea711cb6661665
Joseito <jgonzar09@gmail.com>
2022-10-11 22:04:22 -0400

    setting up styles and layout

---
+++ src/components/TodoCounter.js
@@ -0,0 +1,10 @@
+import '../styles/TodoCounter.css';
+import React from "react";
+
+function TodoCounter(){
+    return (
+      <h2>Has completado 2 de 3 tareas</h2>
+    );
+}
+
+export { TodoCounter };
---
+++ src/components/TodoSearch.js
@@ -0,0 +1,10 @@
+import '../styles/TodoSearch.css';
+import React from 'react';
+
+function TodoSearch(){
+    return(
+        <input placeholder='Create new task' />  
+    )
+}
+
+export { TodoSearch };
